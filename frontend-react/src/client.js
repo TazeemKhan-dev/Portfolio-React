a6d2f7ac36e3071749271ec014da7ca17d188475
Tazeem khan <tazeemkhan0712@gmail.com>
2023-11-08 05:46:44 +0530

    added the keys correctly

--- frontend-react/src/client.js
+++ frontend-react/src/client.js
@@ -1,15 +1,14 @@
 import { createClient } from "@sanity/client";
-import imageUrlBuilder from '@sanity/image-url';
+import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: "6wzsf1yl",
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2023-11-07",
   useCdn: true,
-  token:
-    "skNQJyVueBr0hEcsmfvPa2ahhph0eouhdUYLAuhsZ7wwtdIe8LMidKdgcgtP2wYwMNRcyPkE6qMNnUwuQDo3khONeUQYu6MlqSAY7cyEDaPNsCd8MojckqrYE9PwPKUvj18kJPqCrtZ20nSC0lQdPQHygRlydZZBrdhgMI9s1nFXOQ9qWeJT",
+  token: import.meta.env.VITE_SANITY_TOKEN,
 });
-
+// console.log(process.env.REACT_APP_SANITY_PROJECT_ID);
 const builder = imageUrlBuilder(client);
 
 export const urlFor = (source) => builder.image(source);
